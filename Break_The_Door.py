e85c626ca6d290dfa55fce70139474446da0ed05
Funtaztic <abelszabolcsi@gmail.com>
2016-09-29 21:26:57 +0200

    ver 1.2

--- Break_The_Door.py
+++ Break_The_Door.py
@@ -1,19 +1,28 @@
 # Break-The-Door_1.0 - Base version, with a variable for door_health and and
 #                      small damages.
 # Break-The-Door_1.1 - If door breaks, print something. -> while True (?)
+# Break-The-Door_1.2 - based on The_Closed_Door1.2.py, it will have two options, but eventually one will make the other work. Removed while.
 
 print "This is the Break-The-Door Game!"
 door_health = 100
 
 def Break_The_Door():
     global door_health
-    while True:
-        n = raw_input("Do you want to damage it? Health: %d" (door_health))
-        if n.strip() == "yes":
-            door_health -= 10
-        elif n.strip() == "no":
-            print "Door is at %d" (door_health)
-        elif door_health == 0:
-            break
+    n = raw_input("Do you want to damage it?")
+    if n == "yes":
+        door_health -= 10
+        print "Door health: %r" % (door_health)
+        Break_The_Door()
+    elif n == "no":
+        print "The door stays intact."
+        print "Door health: %r" % (door_health)
+        Break_The_Door()
 
-Break_The_Door()
+def restart():
+    if door_health > 0:
+        Break_The_Door()
+    else:
+        print "You broke the door! Enter some text and enter to quit."
+        exit = raw_input
+
+restart()
